9a826f72d94c80efa4d1fb6c6d22c4ef30939db3
songjihu <275245802@qq.com>
2019-03-13 22:08:27 +0800

    first_add

---
+++ app/src/main/java/Entity/UserInfo.java
@@ -0,0 +1,33 @@
+package Entity;
+
+public class UserInfo {
+    private String userid;
+    private String userpwd;
+    private String rpwd;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getUserpwd() {
+        return userpwd;
+    }
+
+    public String getRpwd() {
+        return rpwd;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public void setUserpwd(String userpwd) {
+        this.userpwd = userpwd;
+    }
+
+    public void setRpwd(String rpwd) {
+        this.rpwd = rpwd;
+    }
+
+
+}
---
+++ app/src/main/java/MyThread/LoginThread.java
@@ -0,0 +1,70 @@
+package MyThread;
+
+import android.util.Log;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Entity.UserInfo;
+
+
+public class LoginThread extends Thread {
+    public UserInfo getUu() {
+        return uu;
+    }
+
+    public void setUu(UserInfo uu) {
+        this.uu = uu;
+    }
+
+    private UserInfo uu;
+
+
+
+    public LoginThread(String id, String password)
+    {
+        uu.setUserid(id);
+        uu.setUserpwd(password);
+
+    }
+    public void run()
+    {
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            java.sql.Connection cn= DriverManager.getConnection("jdbc:mysql://182.254.161.189/kcsj","root","mypwd");
+            String sql="SELECT userpwd FROM `user` WHERE userid = "+uu.getUserid();
+            Statement st=(Statement)cn.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+            while(rs.next()){
+                uu.setRpwd(rs.getString("userpwd"));
+
+                Log.i("LoginActivity",uu.getRpwd());
+            }
+            cn.close();
+            st.close();
+            rs.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println("hello " + name);
+    }
+/*
+    public String getRt() {
+        if(this.rpwd.equals(this.userpwd))
+            return this.userpwd;
+        else
+            return this.rpwd;
+    }
+*/
+    public static void main(String[] args)
+    {
+
+    }
+}
